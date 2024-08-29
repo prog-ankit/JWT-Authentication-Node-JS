f459b4ef4ffd96de639f77ccc4982c7114247a5c
Ankit Bose <abinfo2310@gmail.com>
2024-08-30 01:48:12 +0530

    Refresh Token Code Added

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ const defaultuser = [
     isAdmin: false,
   },
 ];
+const refreshTokens = [];
+app.post("/api/refreshToken",(req,res) => {
+  const token = req.body.token;
+  if(!token) res.status(401).json("User is not authorized!");
+  if(!refreshTokens.includes(token)) res.status(403).json("token is invalid!");
+  jwt.verify(token,"refreshTesting",(err,user) => {
+    err && console.log(err);
+    ///removes the current refresh token from refreshTokens array
+    refreshTokens = refreshTokens.filter((refreshToken) => refreshToken !== token);
+    const newAccessToken = jwt.sign({ id : user.id, isAdmin : user.isAdmin}, "testing",{expiresIn : "10s"});
+    const newRefreshToken = jwt.sign({id: user.id, isAdmin: user.isAdmin}, "refreshTesting");
+    refreshTokens.push(newRefreshToken);
+
+    return res.status(200).json({
+      newAccessToken : newAccessToken,
+      newRefreshToken : newRefreshToken
+    });
+  })
+});
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
   const user = defaultuser.find(obj => {
@@ -23,10 +42,14 @@ app.post("/api/login", (req, res) => {
   });
   if(user) {
     const accessToken = jwt.sign({ id : user.id, isAdmin : user.isAdmin}, "testing",{expiresIn : "10s"});
+    const refreshToken = jwt.sign({id: user.id, isAdmin: user.isAdmin}, "refreshTesting");
+    refreshTokens.push(refreshToken);
+
     res.json({
       username : user.username,
       isAdmin : user.isAdmin,
-      accessToken
+      accessToken,
+      refreshToken
     })
   } else {
     res.status(400).json("User Not Found.");
