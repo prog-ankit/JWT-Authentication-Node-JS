e4cf9c2d1c674e476841e99ddd91c40407c12d16
Ankit Bose <abinfo2310@gmail.com>
2024-09-01 22:47:18 +0530

    API Side JWT Authentication Done

--- index.js
+++ index.js
@@ -16,71 +16,99 @@ const defaultuser = [
     isAdmin: false,
   },
 ];
-const refreshTokens = [];
-app.post("/api/refreshToken",(req,res) => {
+let refreshTokens = [];
+app.post("/api/refreshToken", (req, res) => {
   const token = req.body.token;
-  if(!token) res.status(401).json("User is not authorized!");
-  if(!refreshTokens.includes(token)) res.status(403).json("token is invalid!");
-  jwt.verify(token,"refreshTesting",(err,user) => {
+  if (!token) res.status(401).json("User is not authorized!");
+  if (!refreshTokens.includes(token))
+    res.status(403).json("Refresh token is not valid");
+  jwt.verify(token, "refreshTesting", (err, user) => {
     err && console.log(err);
     ///removes the current refresh token from refreshTokens array
-    refreshTokens = refreshTokens.filter((refreshToken) => refreshToken !== token);
-    const newAccessToken = jwt.sign({ id : user.id, isAdmin : user.isAdmin}, "testing",{expiresIn : "10s"});
-    const newRefreshToken = jwt.sign({id: user.id, isAdmin: user.isAdmin}, "refreshTesting");
+    refreshTokens = refreshTokens.filter(
+      (refreshToken) => refreshToken !== token
+    );
+    const newAccessToken = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      "testing",
+      { expiresIn: "60s" }
+    );
+    const newRefreshToken = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      "refreshTesting"
+    );
     refreshTokens.push(newRefreshToken);
 
     return res.status(200).json({
-      newAccessToken : newAccessToken,
-      newRefreshToken : newRefreshToken
+      newAccessToken: newAccessToken,
+      newRefreshToken: newRefreshToken,
     });
-  })
+  });
 });
+
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
-  const user = defaultuser.find(obj => {
-    return obj.username === username && obj.password === password
+  const user = defaultuser.find((obj) => {
+    return obj.username === username && obj.password === password;
   });
-  if(user) {
-    const accessToken = jwt.sign({ id : user.id, isAdmin : user.isAdmin}, "testing",{expiresIn : "10s"});
-    const refreshToken = jwt.sign({id: user.id, isAdmin: user.isAdmin}, "refreshTesting");
+  if (user) {
+    const accessToken = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      "testing",
+      { expiresIn: "60s" }
+    );
+    const refreshToken = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      "refreshTesting"
+    );
     refreshTokens.push(refreshToken);
 
     res.json({
-      username : user.username,
-      isAdmin : user.isAdmin,
+      username: user.username,
+      isAdmin: user.isAdmin,
       accessToken,
-      refreshToken
-    })
+      refreshToken,
+    });
   } else {
     res.status(400).json("User Not Found.");
   }
   res.json("Hello, Testing going on!!");
 });
 
-const verify = (req,res,next) => {
+const verify = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if(authHeader) {
+  if (authHeader) {
     const token = authHeader.split(" ")[1];
-    jwt.verify(token,"testing",(err,user) => {
-      if(err){
+    jwt.verify(token, "testing", (err, user) => {
+      if (err) {
+        console.log(err);
         return res.status(403).json("Token is not valid");
       } else {
         req.user = user;
         next();
       }
     });
-    console.log(token);
   } else {
     return res.status(401).json("User is unauthorized!!");
   }
-}
-app.delete("/api/users/:userId",verify,(req,res) => {
-  if(req.user.id === req.params.userId || req.user.isAdmin) {
+};
+
+app.post("/api/users/logout", verify, (req, res) => {
+  const token = req.body.token;
+  refreshTokens = refreshTokens.filter(
+    (refreshToken) => refreshToken !== token
+  );
+  res.status(200).json("You have been logged out!!");
+});
+
+app.delete("/api/users/:userId", verify, (req, res) => {
+  if (req.user.id === req.params.userId || req.user.isAdmin) {
     res.status(200).json("User is deleted!!");
   } else {
     res.status(404).json("User does not exist or you are not admin.");
   }
-})
+});
+
 app.listen(5000, () => {
   console.log("Server is Running on port 5000.");
 });
